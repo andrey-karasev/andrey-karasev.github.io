82dc932d0f04790de4f2b0a198fcba4988edc828
Andrey Karasev <andrey.karasev.ext@bayer.com>
2025-02-01 20:36:30 -0500

    Trying to create playlist

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 import {readdirSync, writeFileSync} from 'fs';
 
-const list = new Array<string>();
-
+let result = '#EXTM3U\n';
 readdirSync('/Users/andrey.karasev/Sources/andrey-karasev/andrey-karasev.github.io/music/RobDouganFilmsPastAndFuture2018')  
   .forEach((file) => {
-    list.push(`https:/andrey-karasev.github.io/music/RobDouganFilmsPastAndFuture2018/${encodeURIComponent(file)}`)
+    result += 
+    `#EXTINF:123, ${file.replace('.mp3','')}
+  https:/andrey-karasev.github.io/music/RobDouganFilmsPastAndFuture2018/${encodeURIComponent(file)}
+  
+  `;
+    
   });
 
- writeFileSync('RobDouganFilmsPastAndFuture2018.m3u', list.join('\n'));
+  
+
+
+  writeFileSync('RobDougan.m3u', result);
